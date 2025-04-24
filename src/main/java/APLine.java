36d5b3e5171fb2e453c42751aadfb6d3f464b290
alexaBernard915 <albernard@s.sfusd.edu>
2025-04-24 10:36:36 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -4,7 +4,7 @@ public class APLine
   private int b;
   private int c; 
   private double slope; 
-  public APLine(int a1, b1, c1){
+  public APLine(int a1, int b1, int c1){
     a = a1; 
     b = b1; 
     c = c1; 
